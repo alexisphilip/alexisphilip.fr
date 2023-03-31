2f0b8c896bbdbed0a421edcf74f2046005efbf61
Alexis Philip <alexis.philip13@gmail.com>
2023-03-31 10:57:25 +0200

    Updated Casio F-91W

--- content/demo/Casio-F-91W/script/CasioF91W.js
+++ content/demo/Casio-F-91W/script/CasioF91W.js
@@ -4,22 +4,6 @@
  */
 
 /**
- * TODO:
- * - NAMING: set new names for the different segments displays (no snake_case?)
- * - FIXES:
- *      - see TODOs
- *      - RESPONSIVE (phone view is horrible)
- *      - when incrementing values, do not increment the higher value after (ex: 59 minutes -> 00 should not increment the hour)
- *      - alarm get/set should take into account time mode 12h
- * - FUNCTIONALITIES:
- *      - when a mode is triggered, display on the side it's current mode and instructions
- *      - display basic instructions on load?
- *      - displays instructions next to buttons dynamically
- *      - add click sound and alarms
- *      - store Casio data in cookies so the user has its settings saved
- */
-
-/**
  * Casio F-91W.
  * 
  * I like to to see this class as the "case" of the watch.
@@ -35,7 +19,7 @@
  * 
  * @author Alexis Philip (alexisphilip.fr)
  */
- class CasioF91W {
+class CasioF91W {
     /**
      * The watch's operating system object.
      * @var {CasioF91WOperatingSystem}
